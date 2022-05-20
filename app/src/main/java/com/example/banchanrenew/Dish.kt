41c8c621e583b9befcb30d41b8ce1e4f6dd3074e
Minchae0322 <wjdalsco12>
2022-05-20 18:16:53 +0900

    2022-5-20

---
+++ app/src/main/java/com/example/banchanrenew/Dish.kt
@@ -0,0 +1,20 @@
+package com.example.banchanrenew
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Dish")
+class Dish(
+    @PrimaryKey val name: String,
+    @ColumnInfo(name = "dishId") val dishId: Int,
+    @ColumnInfo(name = "explain") var explain: String?,
+    @ColumnInfo(name = "nation") var nation: String?,
+    @ColumnInfo(name = "type") var type: String?,
+    @ColumnInfo(name = "cookingTime") var cookingTime: String?,
+    @ColumnInfo(name = "calorie") var calorie: String?,
+    @ColumnInfo(name = "dishImage") var dishImage: Int,
+
+
+    )    {
+}
